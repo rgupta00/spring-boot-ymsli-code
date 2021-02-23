782803c0e1acebd715d27d021e3e28c887c03e1e
rgupta00 <rgupta.mtech@gmail.com>
2021-02-23 09:54:08 +0530

    rest hello

---
+++ mvcrest/src/main/java/com/bookapp/controller/BookRestController.java
@@ -0,0 +1,83 @@
+package com.bookapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookapp.model.dao.Book;
+import com.bookapp.model.service.BookService;
+
+@RestController
+@RequestMapping(path = "api")
+public class BookRestController {
+
+	private BookService bookService;
+
+	@Autowired
+	public BookRestController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@GetMapping(path = "book", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Book> getAllBooks(){
+		return bookService.getAllBooks();
+	}
+	
+	@GetMapping(path = "book/{bid}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Book getAnBook(@PathVariable(name="bid") int bookId){
+		return bookService.getBookById(bookId);
+	}
+	@PostMapping(path="book", 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes =  MediaType.APPLICATION_JSON_VALUE)
+	
+	public Book addBook( @RequestBody Book book) {
+		return bookService.addBook(book);
+	}
+	
+	@PutMapping(path="book/{id}", 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes =  MediaType.APPLICATION_JSON_VALUE)
+	public Book updateBook(@PathVariable(name = "id") int id,  @RequestBody Book book) {
+		return bookService.updateBook(id, book);
+	}
+	
+	@DeleteMapping(path="book/{id}", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Book deleteBook(@PathVariable(name = "id") int id) {
+		return bookService.removeBook(id);
+	}
+}
+
+
+/*
+ * @PathVariable vs @RequestParam
+ * @PathVariable: is used to get varable part of the path
+ * 
+ * http://localhost:8080/mvcrest/api/book/1/author/1
+ */
+
+
+/*
+ * 
+ * @RequestBody: it used to tell spring to convert the recived json to java object
+ * @RequestBody: it is used to tell sring to convert java to json object
+ * 
+ * @RestController=@Controller+ @ResponseBody(it invoke parser and use it to conver java ---> json
+ */
+
+
+
+
+
+
+
---
+++ mvcrest/src/main/java/com/bookapp/model/service/BookServiceImpl.java
@@ -0,0 +1,51 @@
+package com.bookapp.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bookapp.model.dao.Book;
+import com.bookapp.model.dao.BookDao;
+@Transactional
+@Service
+public class BookServiceImpl implements BookService {
+
+	private BookDao bookDao;
+
+	@Autowired
+	public BookServiceImpl(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		return bookDao.getAllBooks();
+	}
+
+	@Override
+	public Book getBookById(int bookId) {
+		return bookDao.getBookById(bookId);
+	}
+
+	@Override
+	public Book addBook(Book book) {
+		return bookDao.addBook(book);
+		
+	}
+
+	@Override
+	public Book updateBook(int id, Book book) {
+		Book bookToUpdate=bookDao.getBookById(id);
+		bookToUpdate.setPrice(book.getPrice());
+		//
+		return bookDao.updateBook(bookToUpdate);
+	}
+
+	@Override
+	public Book removeBook(int bookId) {
+		return bookDao.removeBook(bookId);
+	}
+
+}
